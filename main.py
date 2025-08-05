5e1b9889b4d92c6d22f319f079bb725eb4512397
bryanMO18 <l23121066@morelia.tecnm.mx>
2025-08-05 06:53:36 -0600

    Estructura inicial del proyecto Gestor de tareas

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    print("Bienvenido al gestor de tareas")
+    print("1.- Agregar tareas")
+    print("2.- Listar tareas")
+    print("3.- Marcar tarea como completada")
+    print("4.- Salir")
+    if __name__ == "__main__":
+        main()
